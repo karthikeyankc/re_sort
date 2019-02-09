4d65ba0fd53c179bc6484faa825a39dc34999410
karthikeyankc <karthikeyankc@live.com>
2019-02-09 20:01:18 +0530

    Initial commit!

---
+++ README.md
@@ -0,0 +1,8 @@
+# re_sort
+A python script that takes a list of references from a word document and sorts it based on the year it was published.
+
+# Usage
+Clone the repo. Rename your document to `references.docx`, place it in the folder, and run the script.
+
+# Note
+Styles won't be preserved. ¯\_(ツ)_/¯
---
+++ sort.py
@@ -0,0 +1,36 @@
+import re
+from collections import defaultdict, OrderedDict
+from docx import Document
+
+''' Open the word document '''
+
+document = Document('references.docx')
+references = defaultdict(list) # Using lists to hold values under the same year as the key.
+
+''' For every paragraph in the document try to find any four digit number
+	where the first digit should either be 1 or 2, and the third and fourth
+	digits are anything from 0 - 9, and add it to the references dictionary
+	with year as the key.
+''' 
+for line in document.paragraphs:
+	line = line.text.encode('ascii', 'ignore').decode('ascii')
+	match = re.search(r'([1-2][0-9]{3})', line)
+	
+	if match:
+		year = match.group(1)
+	else:
+		year = "0000"
+	
+	references[year].append(line)
+
+''' Sort by keys '''
+ordered_references = OrderedDict(sorted(references.items(), reverse=True))
+
+''' Write the sorted values to a new document. '''
+new_document = Document()
+
+for year, references in ordered_references.iteritems():
+	for reference in references:
+		new_document.add_paragraph(reference)
+
+new_document.save('sorted_references.docx')
